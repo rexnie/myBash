74d38f644897e8af3ab7d4708afd574b402468b1
rex <niedao516@126.com>
2014-05-10 21:53:00 +0800

    add read/echo sample code

---
+++ hello_sh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "input your first,last name:" first_name last_name
+echo "your first name is:$first_name"
+echo "your last name is:"$last_name
+echo -n "your full name is $first_name,$last_name"
